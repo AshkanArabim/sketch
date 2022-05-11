dfe64b7174cd90d876acaf66112e23cca039d116
Ashkan <ashkan.arabim@gmail.com>
2022-05-10 20:54:32 -0600

    add canvas generator button

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function canvas(l) {
-  if (!(0 <= l && l <= 100)) {
-    console.log("Length of canvas should be between 0 and 100");
+const canvasser = document.querySelector("#canvasser");
+
+function canvas() {
+  let length = prompt("Enter length of canvas side: (from 0 to 100)");
+  if (!(0 <= length && length <= 100)) {
+    alert("Error: Length of canvas should be between 0 and 100");
     return;
   }
 
-  let a = l * l;
+  let area = length * length;
   let container = document.querySelector(".container");
 
   while (document.querySelector(".container div")) {
@@ -13,12 +16,16 @@ function canvas(l) {
 
   container.setAttribute(
     "style",
-    `grid-template-columns: repeat(${l}, 1fr); grid-template-rows: repeat(${l}, 1fr)`
+    `grid-template-columns: repeat(${length}, 1fr); grid-template-rows: repeat(${length}, 1fr)`
   );
 
-  for (i = 1; i <= a; i++) {
+  for (i = 1; i <= area; i++) {
     let pixel = document.createElement("div");
+    pixel.addEventListener("mouseover", () => {
+      pixel.style.backgroundColor = "#000000";
+    });
     container.appendChild(pixel);
-    // container.appendChild(pixel);
   }
 }
+
+canvasser.addEventListener("click", canvas);
